7d1f1899cac0e266cfba48af482a7eee7ae60734
varunpenumudi <penumudivarun@gmail.com>
2024-01-10 05:36:33 +0530

    Added title to the page and Set the theme of app to light

--- flet_counter_app/main-app.py
+++ flet_counter_app/main-app.py
@@ -1,6 +1,8 @@
 import flet as ft 
 
 def main(page: ft.page):
-    pass
+    page.title = "Flet Counter App"
+    page.theme_mode = "light"
+    page.update()
 
 ft.app(target=main, view=ft.AppView.WEB_BROWSER)
